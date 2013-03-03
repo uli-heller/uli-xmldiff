d5179488ce8edf616fd2f8d7de7ec13b255813e4
Uli Heller <uli.heller@daemons-point.com>
2013-03-03 19:19:26 +0100

    Deactivated groovy for Linux

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -5,23 +5,23 @@ D="$(cd "${D}"; pwd)"
 
 TPD="${D}/../3rd-party"
 
-GROOVY_VERSION="2.1.1"
-GROOVY_ZIP_BASENAME="groovy-binary-${GROOVY_VERSION}.zip"
-GROOVY_ZIP_DOWNLOAD_URL="http://dist.groovy.codehaus.org/distributions/${GROOVY_ZIP_BASENAME}"
-GROOVY_ZIP_ABSOLUTE_PATH="${TPD}/${GROOVY_ZIP_BASENAME}"
-
 if [ ! -d "${TPD}" ]; then
   mkdir -p "${TPD}"
 fi
 
-if [ ! -d "${D}/groovy" ]; then
-  if [ ! -s  "${GROOVY_ZIP_ABSOLUTE_PATH}" ]; then
-    "${D}/httpcat.sh" "${GROOVY_ZIP_DOWNLOAD_URL}" >"${GROOVY_ZIP_ABSOLUTE_PATH}"
-  fi
-  (cd "${D}"; "${D}/myjar.sh" -xf "${GROOVY_ZIP_ABSOLUTE_PATH}")
-  mv "${D}/groovy-${GROOVY_VERSION}" "${D}/groovy"
-  (cd "${D}/groovy/bin"; ls|grep -v "bat"|xargs chmod +x)
-fi
+#GROOVY_VERSION="2.1.1"
+#GROOVY_ZIP_BASENAME="groovy-binary-${GROOVY_VERSION}.zip"
+#GROOVY_ZIP_DOWNLOAD_URL="http://dist.groovy.codehaus.org/distributions/${GROOVY_ZIP_BASENAME}"
+#GROOVY_ZIP_ABSOLUTE_PATH="${TPD}/${GROOVY_ZIP_BASENAME}"
+#
+#if [ ! -d "${D}/groovy" ]; then
+#  if [ ! -s  "${GROOVY_ZIP_ABSOLUTE_PATH}" ]; then
+#    "${D}/httpcat.sh" "${GROOVY_ZIP_DOWNLOAD_URL}" >"${GROOVY_ZIP_ABSOLUTE_PATH}"
+#  fi
+#  (cd "${D}"; "${D}/myjar.sh" -xf "${GROOVY_ZIP_ABSOLUTE_PATH}")
+#  mv "${D}/groovy-${GROOVY_VERSION}" "${D}/groovy"
+#  (cd "${D}/groovy/bin"; ls|grep -v "bat"|xargs chmod +x)
+#fi
 
 XMLUNIT_VERSION="1.4"
 XMLUNIT_BASENAME="xmlunit-${XMLUNIT_VERSION}"
